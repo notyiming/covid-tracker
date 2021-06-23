b1154b19b4d8c245734dd49f73f189ebdc712fe7
yiming <ymtan.2062@gmail.com>
2021-06-23 21:27:09 +0100

    initial commit, first basic working version

---
+++ public/javascripts/googlemap.js
@@ -0,0 +1,36 @@
+function initMap() {
+    var mapOptions = {
+        zoom: 3,
+        center: new google.maps.LatLng(20, 0),
+        mapTypeId: 'satellite'
+    };
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    //filling in the numbers for totals
+    $.getJSON("https://corona.lmao.ninja/v3/covid-19/all", function (data){
+        $("#total-cases").html(data.cases);
+        $("#total-deaths").html(data.deaths);
+        $("#total-recovered").html(data.recovered);
+    });
+
+
+    // populating the dom with all the countries
+    $.getJSON("https://corona.lmao.ninja/v3/covid-19/countries", function (data){
+        for(let i = 0; i < data.length; i++){
+            let date = new Date(data[i].updated);
+            $("#all-countries").append("<div class=\"col-xl-3 p-3\">\n" +
+                "    <div class=\"card h-100\">\n" +
+                "      <div class=\"card-body\">\n" +
+                "        <h5 class=\"card-title\">"+data[i].country+"</h5>\n" +
+                "        <p class=\"card-text\"> Total Cases: "+ data[i].cases + "</p>\n" +
+                "        <p class=\"card-text\">Deaths: "+ data[i].deaths + "</p>\n" +
+                "        <p class=\"card-text\">Recovered: "+ data[i].recovered + "</p>\n" +
+                "      </div>\n" +
+                "      <div class=\"card-footer\">\n" +
+                "        <small class=\"text-muted\">Last updated: "+ date.toLocaleString() +"</small>\n" +
+                "      </div>\n" +
+                "    </div>\n" +
+                "  </div>");
+        }
+    });
+}
