1fda8d4fa447b8bf1deb604ec9666299ad898d72
yiming <ymtan.2062@gmail.com>
2021-06-26 16:08:17 +0100

    implemented live map with stats, changed some sytling, fixed responsive on mobile issue, linked about me page to personal website

--- public/javascripts/getAllStats.js
+++ public/javascripts/getAllStats.js
@@ -1,5 +1,5 @@
 //filling in the numbers for totals
-$.getJSON("https://corona.lmao.ninja/v3/covid-19/all", function (data){
+$.getJSON("https://disease.sh/v3/covid-19/all", function (data){
     $("#total-cases").html(data.cases);
     $("#total-deaths").html(data.deaths);
     $("#total-recovered").html(data.recovered);
@@ -7,13 +7,14 @@ $.getJSON("https://corona.lmao.ninja/v3/covid-19/all", function (data){
 
 
 // populating the dom with all the countries
-$.getJSON("https://corona.lmao.ninja/v3/covid-19/countries", function (data){
+$.getJSON("https://disease.sh/v3/covid-19/countries", function (data){
     for(let i = 0; i < data.length; i++){
         let date = new Date(data[i].updated);
         $("#all-countries").append("<div class=\"col-xl-3 col-sm-12 p-3\">\n" +
-            "    <div class=\"card h-100\">\n" +
+            "    <div class=\"card text-white bg-dark-card h-100\">\n" +
+            // "      <img height='100px' class=\"card-img-top\" src=\""+ data[i].countryInfo.flag +"\" alt=\"Card image cap\">" +
             "      <div class=\"card-body\">\n" +
-            "        <h5 class=\"card-title\">"+data[i].country+"</h5>\n" +
+            "        <h5 class=\"card-title\"><span>"+data[i].country+"</span><img width='35px' height='25px' class=\"float-end\" src=\""+ data[i].countryInfo.flag +"\" alt=\"Card image cap\"></h5>\n" +
             "        <p class=\"card-text\"> Total Cases: "+ data[i].cases + "</p>\n" +
             "        <p class=\"card-text\">Deaths: "+ data[i].deaths + "</p>\n" +
             "        <p class=\"card-text\">Recovered: "+ data[i].recovered + "</p>\n" +
--- public/javascripts/googlemap.js
+++ public/javascripts/googlemap.js
@@ -1,8 +1,129 @@
 function initMap() {
     var mapOptions = {
-        zoom: 3,
-        center: new google.maps.LatLng(20, 0),
-        mapTypeId: 'satellite'
+        zoom:5,
+        maxZoom: 6,
+        minZoom:3,
+        center: new google.maps.LatLng(50, 10),
+        disableDefaultUI: true,
+        // mapId: "8e0a97af9386fef",
+        styles: [
+            { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+            { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+            { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+            {
+                featureType: "administrative.locality",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#d59563" }],
+            },
+            {
+                featureType: "administrative.country",
+                elementType: "geometry.stroke",
+                stylers: [{ color: "#bf937e" }, {weight: 2}],
+            },
+            {
+                featureType: "poi",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#d59563" }],
+            },
+            {
+                featureType: "poi.park",
+                elementType: "geometry",
+                stylers: [{ color: "#263c3f" }],
+            },
+            {
+                featureType: "poi.park",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#6b9a76" }],
+            },
+            {
+                featureType: "road",
+                elementType: "geometry",
+                stylers: [{ color: "#38414e" }],
+            },
+            {
+                featureType: "road",
+                elementType: "geometry.stroke",
+                stylers: [{ color: "#212a37" }],
+            },
+            {
+                featureType: "road",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#9ca5b3" }],
+            },
+            {
+                featureType: "road.highway",
+                elementType: "geometry",
+                stylers: [{ color: "#746855" }],
+            },
+            {
+                featureType: "road.highway",
+                elementType: "geometry.stroke",
+                stylers: [{ color: "#1f2835" }],
+            },
+            {
+                featureType: "road.highway",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#f3d19c" }],
+            },
+            {
+                featureType: "transit",
+                elementType: "geometry",
+                stylers: [{ color: "#2f3948" }],
+            },
+            {
+                featureType: "transit.station",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#d59563" }],
+            },
+            {
+                featureType: "water",
+                elementType: "geometry",
+                stylers: [{ color: "#17263c" }],
+            },
+            {
+                featureType: "water",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#515c6d" }],
+            },
+            {
+                featureType: "water",
+                elementType: "labels.text.stroke",
+                stylers: [{ color: "#17263c" }],
+            },
+        ]
     };
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    $.getJSON("https://corona.lmao.ninja/v3/covid-19/countries", function (data){
+        for(let i = 0; i < data.length; i++){
+            let lat = data[i].countryInfo.lat;
+            let long = data[i].countryInfo.long;
+            let content = "<h2>"+ data[i].country +"</h2>" +
+                "<h5>Total Cases: "+ data[i].cases +"</h5>" +
+                "<h5>New Cases: +"+ data[i].todayCases +"</h5>"
+            let icon = {
+                url:data[i].countryInfo.flag,
+                scaledSize: new google.maps.Size(30,20)
+            }
+            addCountryMarker(lat, long,content, icon)
+        }
+    });
+
+
+
+    function addCountryMarker(lat, long, content, icon){
+        var marker = new google.maps.Marker({
+            position:{lat:lat,lng:long},
+            map:map,
+            icon:icon
+        });
+
+        var statPopup = new google.maps.InfoWindow({
+            content:content
+        });
+
+        marker.addListener('click', function(){
+            statPopup.open(map, marker);
+        })
+    }
 }
